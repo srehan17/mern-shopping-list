ae4b0a65358b98bd2140516162b9b12969421e0c
Sarah <sarahahmad17@gmail.com>
2020-10-01 12:43:58 -0400

    setting up to deploy on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -19,6 +20,16 @@ mongoose
 
 // use routes
 app.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === production) {
+  // Then set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__direname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
