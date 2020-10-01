85cc2a75fe01d4e69ea8549ebb54390f1b7a5371
Sarah <sarahahmad17@gmail.com>
2020-10-01 00:31:43 -0400

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://sarah:breaking@cluster0.eowli.mongodb.net/shopping_db?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// BodyParser Middleware
+app.use(express.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+// use routes
+app.use("/api/items", items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
